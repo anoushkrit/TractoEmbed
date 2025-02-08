82dd365ee3cbb7cbee44353a7661f807fc9d07b5
BipanjitGill <120119876+BipanjitGill@users.noreply.github.com>
2025-02-08 12:58:32 +0100

    Added HCP mass center

--- train_test/test_multiembed_realdata.sh
+++ train_test/test_multiembed_realdata.sh
@@ -1,8 +1,8 @@
-model_weight_path="" 
+model_weight_path=""  # path to folder container model weights and args
 test_realdata_batch_size=512
-input_data_path=""
-HCP_center_path="HCP_mass_center.npy"
-output_trk_path=""
+input_data_path="" # path to input trk file
+HCP_center_path="../dataloader/HCP_mass_center.npy"
+output_trk_path="" # path to save output vtk of each class
 CUDA_VISIBLE_DEVICES=0 python test_realdata.py --out_path_base ${out_path} \
                --test_realdata_batch_size ${test_realdata_batch_size} \
                --tractography_path ${input_data_path}\
